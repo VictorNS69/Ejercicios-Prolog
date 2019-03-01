965c29a4d817750fbb0b97c61a0647a73fd553be
Víctor Nieves Sánchez <vnievess@gmail.com>
2019-03-01 20:42:22 +0100

    added some operations

--- operaciones-matematicas/operaciones.pl
+++ operaciones-matematicas/operaciones.pl
@@ -6,6 +6,12 @@ nat(s(X)) :-
 % add(X,Y,Z)/3 --> Z = X+Y
 add(0,X,X) :- 			% 0+X=X
 	nat(X).		
-add(s(X),Y,s(Z)) :- 		% (1+X)+Y=(1+Z)
-	add(X,Y,Z). 		
+add(s(X),Y,Z) :- 		% (1+X)+Y=X+(Y+1)
+	add(X,s(Y),Z). 		
+
+% times(X,Y,Z)/3 --> Z=X*Y
+times(0,X,0).                  % 0*X=0
+times(s(X),Y,Z) :-             % (X+1)*Y=(X*Y)+Y
+       times(X,Y,A),
+       add(Y,A,Z).
 
