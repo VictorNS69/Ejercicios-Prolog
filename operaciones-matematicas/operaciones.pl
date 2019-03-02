e9b42638f1b19aab601e4f92d8d55e6713de29eb
Victor Nieves Sanchez <vnievess@gmail.com>
2019-03-02 13:48:48 +0100

    added factorial

--- operaciones-matematicas/operaciones.pl
+++ operaciones-matematicas/operaciones.pl
@@ -4,14 +4,21 @@ nat(s(X)) :-
 	nat(X).
 
 % addition(X,Y,Z)/3 --> Z = X+Y
-addition(0,X,X) :- 			% 0+X=X
-	nat(X).		
-addition(s(X),Y,Z) :- 			% (1+X)+Y=X+(Y+1)
-	addition(X,s(Y),Z). 		
+addition(0,X,X).  			% 0+X=X
+addition(s(X),Y,Z) :- 	% (1+X)+Y=X+(Y+1)
+	addition(X,s(Y),Z).
 
 % multiply(X,Y,Z)/3 --> Z=X*Y
-multiply(0,X,0).                	  % 0*X=0
-multiply(s(X),Y,Z) :-          		  % (X+1)*Y=(X*Y)+Y
+multiply(0,X,0) :- nat(X).        % 0*X=0
+multiply(s(X),Y,Z) :-		% (X+1)*Y=(X*Y)+Y
 	multiply(X,Y,A),
-	addition(Y,A,Z).
+	addition(A,Y,Z).
 
+% fact(X,Y)/2 --> Y = X!
+fact(0,s(0)).						% 0! = 1
+fact(s(X),Y) :-
+	fact(X,Z),
+	multiply(s(X),Z,Y).
+
+
+% TODO: exp(X,Y,Z), min(X,Y,Z)
