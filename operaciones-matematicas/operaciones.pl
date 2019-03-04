6adcd38bdb1a9e0e726ff0beaeaa21e00ee603a5
Víctor Nieves Sánchez <vnievess@gmail.com>
2019-03-04 14:24:49 +0100

    Update operaciones.pl

--- operaciones-matematicas/operaciones.pl
+++ operaciones-matematicas/operaciones.pl
@@ -5,7 +5,7 @@ nat(s(X)) :-
 
 % addition(X,Y,Z)/3 --> Z = X+Y
 addition(0,X,X).  			% 0+X=X
-addition(s(X),Y,Z) :- 	    % (1+X)+Y=X+(Y+1)
+addition(s(X),Y,Z) :-			% (1+X)+Y=X+(Y+1)
 	addition(X,s(Y),Z).
 
 % multiply(X,Y,Z)/3 --> Z=X*Y
